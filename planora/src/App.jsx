b351e1faab366eab39b356bc2f53f0a7119e13e0
Beyza <beyzaaslan311@gmail.com>
2026-08-27 01:34:43 +0300

    feat: implement todo creation functionality

--- planora/src/App.jsx
+++ planora/src/App.jsx
@@ -6,20 +6,18 @@ import "./App.css";
 import ToDoCreate from "./components/ToDoCreate";
 import ToDoList from "./components/ToDoList";
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
 
+  const createTodo = (newTodo) => {
+    //burda şu mantık var önceki todoları bana aç ve üzerine yeni todo ekle mantıgında aşağıdaki kod
+    setTodos([...todos, newTodo]);
+  };
+  console.log(todos);
   return (
     <div className="App">
-      <div
-        style={{
-          width: "500px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <ToDoCreate />
+      <div className="main">
+        {/*childdan parentse geçmek için bi props tanımlıyoruz */}
+        <ToDoCreate onCreateTodo={createTodo} />
         <ToDoList />
       </div>
     </div>
--- planora/src/components/ToDoCreate.jsx
+++ planora/src/components/ToDoCreate.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/todo.css";
-function ToDoCreate() {
+
+function ToDoCreate({ onCreateTodo }) {
+  const [newTodo, setNewTodo] = useState(""); //todo nun içeriğini bunda tutucaz
+  const createTodo = () => {
+    if (!newTodo) return;
+
+    const request = {
+      id: Math.floor(Math.random() * 99999999),
+      content: newTodo,
+    };
+    onCreateTodo(request);
+  };
   return (
     <div className="todo-create">
-      <input className="todo-input " type="text" placeholder="Todo giriniz " />
-      <button className="todo-create-button">Todo oluştur</button>
+      <input
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        className="todo-input "
+        type="text"
+        placeholder="Todo giriniz "
+      />
+      <button onClick={createTodo} className="todo-create-button">
+        Todo oluştur
+      </button>
     </div>
   );
 }
